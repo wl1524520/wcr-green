3880a21f6a91f455e452143e3283bc79bbe07143
Wang Lu <wl1524520@gmail.com>
2016-07-26 10:05:44 +0800

    update

--- content-video.php
+++ content-video.php
@@ -4,7 +4,7 @@
  */
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-    <h2><a href="<?php the_permalink() ?>"><?php if(get_the_title($post->ID)) { the_title(); } else { the_time( get_option( 'date_format' ) ); } ?></a></h2>
+    <h1><a href="<?php the_permalink() ?>"><?php if(get_the_title($post->ID)) { the_title(); } else { the_time( get_option( 'date_format' ) ); } ?></a></h1>
     <div class="meta">
         <span class="meta-label"><i class="fa fa-calendar"></i> <?php echo '日期：'; ?> <?php the_time( get_option( 'date_format' ) ); ?></span>
         <span class="meta-label"><i class="fa fa-user"></i> <?php echo '作者：'; ?> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></span>
