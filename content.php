9d8198699311cbcea6c460a2d75e7a76c6294c1f
Wang Lu <wl1524520@gmail.com>
2016-07-31 01:10:50 +0800

    update

--- content.php
+++ content.php
@@ -8,9 +8,9 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
     <h1><a href="<?php the_permalink() ?>"><?php if(get_the_title($post->ID)) { the_title(); } else { the_time( get_option( 'date_format' ) ); } ?></a></h1>
     <div class="meta">
-        <span class="meta-label"><span class="glyphicon glyphicon-calendar"></span> <?php echo '日期：'; ?> <?php the_time( get_option( 'date_format' ) ); ?></span>
-        <span class="meta-label"><span class="glyphicon glyphicon-user"></span> <?php echo '作者：'; ?> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></span>
-        <span class="meta-label"><span class="glyphicon glyphicon-menu-hamburger"></span> <?php echo '分类：'; ?> <?php the_category(', '); ?></span>
+        <span class="meta-label"><span class="glyphicon glyphicon-calendar"></span> <?php echo '日期：'; ?><?php the_time( get_option( 'date_format' ) ); ?></span>
+        <span class="meta-label"><span class="glyphicon glyphicon-user"></span> <?php echo '作者：'; ?><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></span>
+        <span class="meta-label"><span class="glyphicon glyphicon-menu-hamburger"></span> <?php echo '分类：'; ?><?php the_category(', '); ?></span>
         <?php if(function_exists('the_views')) { echo '<span class="meta-label"><span class="glyphicon glyphicon-bookmark"></span> 阅读：';  the_views() ; echo '</span>'; } ?>
     </div>
 
--- single.php
+++ single.php
@@ -13,9 +13,9 @@
                         <article class="singlepost">
                             <h1><a href="<?php the_permalink() ?>"><?php if(get_the_title($post->ID)) { the_title(); } else { the_time( get_option( 'date_format' ) ); } ?></a></h1>
                             <div class="meta">
-                                <span class="meta-label"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> <?php echo '日期：'; ?> <?php the_time( get_option( 'date_format' ) ); ?></span>
-                                <span class="meta-label"><span class="glyphicon glyphicon-user"></span> <?php echo '作者：'; ?> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></span>
-                                <span class="meta-label"><span class="glyphicon glyphicon-menu-hamburger"></span> <?php echo '分类：'; ?> <?php the_category(', '); ?></span>
+                                <span class="meta-label"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> <?php echo '日期：'; ?><?php the_time( get_option( 'date_format' ) ); ?></span>
+                                <span class="meta-label"><span class="glyphicon glyphicon-user"></span> <?php echo '作者：'; ?><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author(); ?></a></span>
+                                <span class="meta-label"><span class="glyphicon glyphicon-menu-hamburger"></span> <?php echo '分类：'; ?><?php the_category(', '); ?></span>
                                 <?php if(function_exists('the_views')) { echo '<span class="meta-label"><span class="glyphicon glyphicon-bookmark"></span> 阅读：';  the_views() ; echo '</span>'; } ?>
                             </div>
                             <div class="single-content"><?php the_content(); ?></div>
