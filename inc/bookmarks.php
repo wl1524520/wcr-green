ec1038554237e18ac10beb0dc80e48a66fb8ff32
Wang Lu <wl1524520@gmail.com>
2017-08-17 10:00:00 +0800

    20170817100000

--- inc/bookmarks.php
+++ inc/bookmarks.php
@@ -4,11 +4,13 @@ function get_the_link_items($id = null){
     $bookmarks = get_bookmarks('orderby=date&category=' .$id );
     $output = '';
     if ( !empty($bookmarks) ) {
-        $output .= '<ul class="link-items fontSmooth">';
+        //$output .= '<ul class="link-items fontSmooth">';
         foreach ($bookmarks as $bookmark) {
-            $output .=  '<li class="link-item"><a class="link-item-inner effect-apollo" href="' . $bookmark->link_url . '" title="' . $bookmark->link_description . '" target="_blank" >'. get_avatar($bookmark->link_notes,64) . '<span class="sitename">'. $bookmark->link_name .'</span></a></li>';
+            $output .=  '<p class="link-item"><a class="link-item-inner effect-apollo" href="'
+                        . $bookmark->link_url . '" title="' . $bookmark->link_description
+                        . '" target="_blank" ><span class="sitename">'. $bookmark->link_name .'</span></a>: '.$bookmark->link_description.'</p>';
         }
-        $output .= '</ul>';
+        //$output .= '</ul>';
     }
     return $output;
 }
@@ -17,7 +19,7 @@ function get_link_items(){
     $linkcats = get_terms( 'link_category' );
     if ( !empty($linkcats) ) {
         foreach( $linkcats as $linkcat){
-            $result .=  '<h3 class="link-title">'.$linkcat->name.'</h3>';
+            $result .=  '<h2 class="link-title">'.$linkcat->name.'</h2>';
             if( $linkcat->description ) $result .= '<div class="link-description">' . $linkcat->description . '</div>';
             $result .=  get_the_link_items($linkcat->term_id);
         }
