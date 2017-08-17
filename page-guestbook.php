9d98de18dd7d1cd578f5fd614a378ca1b7589405
Wang Lu <wl1524520@gmail.com>
2017-08-17 13:21:44 +0800

    20170817132144

--- page-guestbook.php
+++ page-guestbook.php
@@ -10,7 +10,7 @@ Template Name: 留言
 
             <article class="singlepost">
                 <div class="single-content">
-                    <?php if (have_posts()) : the_post(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
                     <h1 class="text-center"><?php the_title(); ?></h1>
                     <hr>
                     <?php
@@ -20,7 +20,7 @@ Template Name: 留言
                     <p>本博客于2014年11月11日建立，据不完全统计，博主已经写了<?php echo $published_posts ?>篇文章，
                         收获了<?php echo get_comments(array('count'=>true)); ?>条评论，
                         一共坚持了<?php echo floor((time()-strtotime("2014-11-11"))/86400); ?>天。</p>
-                    <?php endif; ?>
+                    <?php endwhile; ?>
                 </div>
             </article>
             <?php comments_template(); ?>
--- page.php
+++ page.php
@@ -10,11 +10,13 @@
 
             <article class="singlepost">
                 <div class="single-content">
-                    <?php if (have_posts()) : the_post(); ?>
+
+                    <?php while ( have_posts() ) : the_post(); ?>
                     <h1 class="text-center"><?php the_title(); ?></h1>
                     <hr>
                     <?php the_content(); ?>
-                    <?php endif; ?>
+                    <?php endwhile; ?>
+                    
                 </div>
             </article>
             <?php comments_template(); ?>
