3c5a8cc638046a6551d6559facc140599aa9624e
Wang Lu <wl1524520@gmail.com>
2017-08-18 10:54:11 +0800

    20170818105411

--- page-guestbook.php
+++ page-guestbook.php
@@ -13,13 +13,14 @@ Template Name: 留言
                     <?php while ( have_posts() ) : the_post(); ?>
                     <h1 class="text-center"><?php the_title(); ?></h1>
                     <hr>
+                    <?php the_content(); ?>
                     <?php
                     $count_posts = wp_count_posts();
                     $published_posts = $count_posts->publish;
                     ?>
-                    <p>本博客于2014年11月11日建立，据不完全统计，博主已经写了<?php echo $published_posts ?>篇文章，
+                    <p><strong>本博客于2014年11月11日建立，据不完全统计，博主已经写了<?php echo $published_posts ?>篇文章，
                         收获了<?php echo get_comments(array('count'=>true)); ?>条评论，
-                        一共坚持了<?php echo floor((time()-strtotime("2014-11-11"))/86400); ?>天。</p>
+                        一共坚持了<?php echo floor((time()-strtotime("2014-11-11"))/86400); ?>天。</strong></p>
                     <?php endwhile; ?>
                 </div>
             </article>
