dca12a2edfb6409f22eddc961925f45a9e4f9750
Wang Lu <wl1524520@gmail.com>
2017-08-18 10:56:06 +0800

    20170818105606

--- page-guestbook.php
+++ page-guestbook.php
@@ -18,6 +18,7 @@ Template Name: 留言
                     $count_posts = wp_count_posts();
                     $published_posts = $count_posts->publish;
                     ?>
+                    <hr>
                     <p><strong>本博客于2014年11月11日建立，据不完全统计，博主已经写了<?php echo $published_posts ?>篇文章，
                         收获了<?php echo get_comments(array('count'=>true)); ?>条评论，
                         一共坚持了<?php echo floor((time()-strtotime("2014-11-11"))/86400); ?>天。</strong></p>
