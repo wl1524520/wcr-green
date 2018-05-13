341263cacb4a43dd8ee1d40dfad7f8af3822da5d
Wang Lu <wl1524520@gmail.com>
2018-05-13 15:08:29 +0800

    update

--- page-bookmarks.php
+++ page-bookmarks.php
@@ -1,12 +1,35 @@
 <?php
 /*
 Template Name: 友情链接
+
+if(function_exists('get_link_items')) echo get_link_items();
 */
  get_header(); ?>
 
 <div class="container">
     <div id="archives">
-        <?php if(function_exists('get_link_items')) echo get_link_items(); ?>
+        <h2 class="link-title text-center">友情链接</h2>
+        <hr>
+        <div class="link-items">
+            <ul>
+                <li><p><a class="bm-link" href="https://www.hm-chn.com/" target="_blank">云南翰墨装饰</a><span class="bm-desc"><span class="bm-dem">：</span>优秀品质 源于专业 承接装修设计与房屋维修</span></p></li>
+                <li><p><a class="bm-link" href="https://webshao.com/" target="_blank">邵维恒博客</a><span class="bm-desc"><span class="bm-dem">：</span>邵维恒博客</span></p></li>
+                <li><p><a class="bm-link" href="https://lianjie.fun/" target="_blank">Lianjie's Blog</a><span class="bm-desc"><span class="bm-dem">：</span>范廉洁的个人博客</span></p></li>
+                <li><p><a class="bm-link" href="http://www.hy-chn.cn/" target="_blank">合一体育</a><span class="bm-desc"><span class="bm-dem">：</span>云南合一体育</span></p></li>
+                <li><p><a class="bm-link" href="http://www.izhuyue.com" target="_blank">FROYO's Blog</a><span class="bm-desc"><span class="bm-dem">：</span>将来的你，一定会感谢现在努力的自己。</span></p></li>
+                <li><p><a class="bm-link" href="https://www.qxfell.com/" target="_blank">小飞熊's blog</a><span class="bm-desc"><span class="bm-dem">：</span>一个爱玩爱折腾从业于IT的小伙子的博客 喜欢Linux 崇尚开源精神</span></p></li>
+                <li><p><a class="bm-link" href="https://oj.leetcode.com/" target="_blank">LeetCode</a><span class="bm-desc"><span class="bm-dem">：</span>LeetCode</span></p></li>
+                <li><p><a class="bm-link" href="http://www.jinbuguo.com/" target="_blank">金步国作品集</a><span class="bm-desc"><span class="bm-dem">：</span>各种有用文档的翻译</span></p></li>
+                <li><p><a class="bm-link" href="http://www.liaoxuefeng.com/" target="_blank">廖雪峰的官方网站</a><span class="bm-desc"><span class="bm-dem">：</span>研究互联网产品和技术，提供原创中文精品教程</span></p></li>
+                <li><p><a class="bm-link" href="http://www.williamlong.info/" target="_blank">月光博客</a><span class="bm-desc"><span class="bm-dem">：</span>青青子衿, 悠悠我心, 但为君故, 沉吟至今</span></p></li>
+                <li><p><a class="bm-link" href="https://www.leavesongs.com/" target="_blank">离别歌</a><span class="bm-desc"><span class="bm-dem">：</span>phithon的小站，长期存在与分享关于网络安全与各种编程的原创文章。</span></p></li>
+                <li><p><a class="bm-link" href="https://iammattd.github.io/" target="_blank">MattD</a><span class="bm-desc"><span class="bm-dem">：</span>一位大牛的博客</span></p></li>
+                <li><p><a class="bm-link" href="http://blog.wpjam.com/" target="_blank">我爱水煮鱼</a><span class="bm-desc"><span class="bm-dem">：</span>人人都爱 WordPress</span></p></li>
+                <li><p><a class="bm-link" href="https://laod.cn" target="_blank">老D博客</a><span class="bm-desc"><span class="bm-dem">：</span>关注谷歌服务，分享互联网精神！</span></p></li>
+                <li><p><a class="bm-link" href="http://www.mintos.org/" target="_blank">薄荷开源网</a><span class="bm-desc"><span class="bm-dem">：</span>致力于Linux推广应用</span></p></li>
+                <li><p><a class="bm-link" href="https://coolshell.cn/" target="_blank">酷壳COOLSHELL</a><span class="bm-desc"><span class="bm-dem">：</span>左耳朵耗子陈皓</span></p></li>
+            </ul>
+        </div>
     </div>
 </div>
 
