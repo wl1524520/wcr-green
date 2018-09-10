931108abc6a5e82d17055243b4416ce55cf4a671
Wang Lu <wl1524520@gmail.com>
2018-09-10 10:28:46 +0800

    update

--- content.php
+++ content.php
@@ -8,8 +8,15 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
     <p>
         <span class="list-date"><?php the_time( get_option( 'date_format' ) ); ?><span>
-        <a class="list-title" href="<?php the_permalink() ?>"><?php if(get_the_title($post->ID)) { the_title(); } else { the_time( get_option( 'date_format' ) ); } ?></a>
-        <span class="list-views"><?php if (function_exists('the_views')) { echo ' (';  the_views() ; echo ' )'; } ?></span>
+        <a class="list-title" href="<?php the_permalink() ?>">
+            <?php 
+                if(get_the_title($post->ID)) { 
+                    the_title(); 
+                } else { 
+                    the_time( get_option( 'date_format' ) ); 
+                } 
+            ?>
+        </a>
         <span class="list-author"><?php echo '&nbsp;&nbsp;-- ';the_author(); ?></span>
     </p>
 </article>
